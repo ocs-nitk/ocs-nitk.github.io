c9737806910fd974ef572060fcdd7406985d3aab
Houki Kohmoli <32835505+kohmoli@users.noreply.github.com>
2020-12-18 16:22:06 +0900

    Add files via upload

---
+++ script/comic.js
@@ -0,0 +1,115 @@
+const ctx = canvas.getContext('2d');
+
+let pages = [];
+let page = 0;
+let pageUnit = 2;
+let book;
+
+window.onload = () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+    
+    const req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open('GET', './json/fileData.json');
+    req.send();
+    req.onload = () =>  {
+        book = req.response.find( book => book.file == location.search.substr(1) );
+        document.title = book.title + " - 漫画研究部";
+        title.innerHTML = book.title;
+        slider.min = 1;
+        slider.max = book.page;
+        slider.value = 1;
+        nowpage.innerHTML = `1/${book.page}`;
+
+        for(var i = 1; i <= book.page; i++){
+            pages[i] = new Image();
+            pages[i].src = `./comic/${book.file}/${i.toString().padStart(2, '0')}.jpg`;
+            pages[i].onload = updateImage;
+        }
+    }
+}
+
+const menu = () => {
+    if(document.getElementsByClassName('hidden').length == 0){
+        header.classList.add('hidden');
+        footer.classList.add('hidden');
+    }else{
+        header.classList.remove('hidden');
+        footer.classList.remove('hidden');
+    }
+    updateImage();
+}
+
+const prev = () => {
+    if(page <= 1){
+        menu();
+        return -1;
+    }
+    
+    page -= pageUnit;
+    slider.value = page;
+    header.classList.add('hidden');
+    footer.classList.add('hidden');
+
+    nowpage.innerHTML = `${slider.value}/${book.page}`;
+    updateImage();
+}
+
+const next = () => {
+    if(page == book.page){
+        menu();
+        return -1;
+    }
+
+    page += pageUnit;
+    slider.value = page;
+    header.classList.add('hidden');
+    footer.classList.add('hidden');
+
+    nowpage.innerHTML = `${slider.value}/${book.page}`;
+    updateImage();
+}
+
+window.onresize = () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+    
+    header.classList.add('hidden');
+    footer.classList.add('hidden');
+
+    updateImage();
+}
+
+const updateImage = () => {
+    if(canvas.height/book.height < canvas.width/book.width/2){
+        pageUnit = 2;
+        page = page - page%2;
+    }else{
+        pageUnit = 1;
+        page = page == 0? 1:page;
+    }
+
+    ctx.fillStyle = '#282828';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    if(pageUnit == 1 || page == book.page){
+        drawPage(page,     1);
+    }else if(page == 0){
+        drawPage(page + 1, 1);
+    }else{
+        drawPage(page,     0);
+        drawPage(page + 1, 2);
+    }
+}
+
+const drawPage = (pi, x) => {
+    const s = Math.min(canvas.width/book.width, canvas.height/book.height);
+    ctx.drawImage(pages[pi], (canvas.width - book.width*s*x)/2, (canvas.height - book.height*s)/2, book.width*s, book.height*s);
+}
+
+slider.oninput = () => {
+    nowpage.innerHTML = `${slider.value}/${book.page}`;
+    page = slider.value - slider.value%pageUnit;
+    updateImage();
+}
---
+++ script/main.js
@@ -0,0 +1,77 @@
+const books = new Vue({
+    el: '#works',
+    data: {
+        books: []
+    },
+    mounted(){
+        const req = new XMLHttpRequest();
+        req.responseType = 'json';
+        req.open('GET', './json/fileData.json');
+        req.onload = () =>  {this.books = req.response;}
+        req.send();
+    },
+    computed: {
+        searchBooks(){
+            const keyword = decodeURI(location.search.substr(8));
+            return this.books.filter(book =>
+                book.title.indexOf(keyword) != -1 ||
+                book.year.toString().indexOf(keyword) != -1 ||
+                book.contents.find(content =>
+                    content.title.indexOf(keyword) != -1 ||
+                    content.author.indexOf(keyword) != -1
+                ) != undefined
+            )
+        }
+    },
+    methods: {
+        link(filename){
+            return `./viewer.html?${filename}`;
+        },
+        thumb(filename){
+            return `./comic/${filename}/01.jpg`;
+        }
+    }
+})
+
+const book = new Vue({
+    el: '#header',
+    computed: {
+        book(){
+            return books.books[0];
+        }
+    },
+    methods: {
+        link(filename){
+            return `./viewer.html?${filename}`;
+        },
+        thumb(filename){
+            return `./comic/${filename}/01.jpg`;
+        }
+    }
+})
+
+window.onscroll = () => {
+    const articles = document.getElementsByTagName('article');
+    for(const a of articles){
+        a.classList.add("hidden");
+    }
+
+    const y = document.documentElement.clientHeight;
+    for(const a of articles){
+        if(a.getBoundingClientRect().top < y*2/3)
+            a.classList.remove("hidden");
+    }
+}
+
+window.onload = () => {
+    const articles = document.getElementsByTagName('article');
+    const y = document.documentElement.clientHeight;
+    for(const a of articles){
+        if(a.getBoundingClientRect().top < y*2/3)
+            a.classList.remove("hidden");
+    }
+
+    if(!navigator.userAgent.match(/iPhone|Android.+Mobile/)){
+        share__line.classList.add('hidden');
+    }
+}
